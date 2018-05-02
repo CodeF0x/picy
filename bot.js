3495219e44ffb22424169b58baac36b053f2a87d
CodeF0x <contact@codef0x.com>
2018-05-02 23:02:25 +0200

    Updated documentation.

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const request = require('request');
 const Telebot = require('telebot');
 
 // Bot
-const bot = new Telebot("");
+const bot = new Telebot("YOUR_API_TOKEN");
 
 // DOMParser
 const jsdom = require('jsdom');
@@ -17,23 +17,23 @@ bot.on('/start', (msg) => msg.reply.text('Hello! For informations about usage, c
 
 // /help command
 bot.on('/help', (msg) => msg.reply.text('Usage:\n /imageof <your_word> sends you an random image.\nExample:\n/imageof dog - You\'ll get an random image of a dog.\n\nAbout:\nVersion: 0.9\nCreator: @CodeFox\nGitHub: https://github.com/CodeF0x/*\n\nMiscellaneous:\nThe images are very tiny, working on a fix.'));
+
 // /imageof command
 bot.on(/^\/imageof (.+)$/, (msg, props) => { 
-    var finalSource;
-    request("https://www.bing.com/search?q=" + props.match[1], function(error, response) {
-        var html = new JSDOM(response.body);
-        var images = html.window.document.getElementsByTagName('img');
-        var sources = [];
-        for (var i = 0; i < images.length; i++) {
+    request("https://www.bing.com/search?q=" + props.match[1], function(error, response) { // Get the search results of bing
+        var html = new JSDOM(response.body); // Parse the response 
+        var images = html.window.document.getElementsByTagName('img'); // Get all images
+        var sources = []; // Array to pick random url from
+        for (var i = 0; i < images.length; i++) { // Loop through all images and push only valid url to the array
             if (images[i].src.includes('http')) {
                 sources.push(images[i].src);
             }
         }
-        sendPhoto(msg, sources[Math.floor(Math.random() * sources.length)]);
+        sendPhoto(msg, sources[Math.floor(Math.random() * sources.length)]); // Random url as parmeter
     });
 });
 
 function sendPhoto(msg, url) {
-    msg.reply.photo(url);
+    msg.reply.photo(url); // Send the photo
 }
 bot.start();
